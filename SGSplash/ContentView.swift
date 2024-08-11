af70b69b5c19c73c63852d70b1c1e4544740f5aa
Xian <hongvanct2003@gmail.com>
2024-08-11 23:37:16 +0700

    Initial Commit

---
+++ SGSplash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SGSplash
+//
+//  Created by Do Le Long An on 11/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SGSplash/SGSplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  SGSplashApp.swift
+//  SGSplash
+//
+//  Created by Do Le Long An on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SGSplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
